b6c08251247d2da3438b92360ca93bf40f00c01e
nate-j-nov <51186042+nate-j-nov@users.noreply.github.com>
2019-05-30 22:24:55 -0400

    Uploaded main cs file for the game

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PokerGame
+{
+
+    //public sealed Stack<T> Deck()
+    /*class Card
+    {
+        Card(string face, string suit);
+    }*/
+
+    public class Deck
+    {
+        string[] faces = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
+            "Nine", "Ten", "Jack", "Queen", "King"};
+        string[] suit = { "Hearts", "Diamonds", "Spades", "Clubs" };
+
+        public Deck()
+        {
+            foreach (string str in suit)
+            {
+                foreach (string k in faces)
+                {
+                    Console.WriteLine(k + " of " + str);
+
+                }
+            }
+        }
+    }
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck d = new Deck();
+        }
+    }
+}
+
