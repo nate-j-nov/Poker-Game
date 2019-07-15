56f75c4e73d1a65368c82daed90a51166cd64821
nate-j-nov <51186042+nate-j-nov@users.noreply.github.com>
2019-07-14 20:35:16 -0400

    Add files via upload

---
+++ Dealer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+using System.Linq;
+using static PokerGame.Card;
+using static PokerGame.CardFace;
+namespace PokerGame
+{
+    public sealed class Dealer
+    {
+        private Stack<Card> deck = new Stack<Card>();
+
+        public Dealer()
+        {
+
+        }
+
+        public void PopulateDeck()
+        {
+            foreach (CardSuit s in Enum.GetValues(typeof(CardSuit)))
+            {
+                foreach (CardFace f in Enum.GetValues(typeof(CardFace)))
+                {
+                    Card c = new Card(f, s);
+                    deck.Push(c);
+                }
+            }
+        }
+
+        //Prints deck
+        public void PrintDeck()
+        {
+            foreach (var v in deck)
+            {
+                Console.WriteLine(v.ToString());
+            }
+        }
+
+        //Shuffles deck
+        public void ShuffleDeck()
+        {
+            Random r = new Random();
+
+            Card[] arrOfCards = deck.ToArray();
+            deck.Clear();
+            for (int x = arrOfCards.Length - 1; x > 0; --x)
+            {
+                int k = r.Next(x + 1);
+                var temp = arrOfCards[x];
+                arrOfCards[x] = arrOfCards[k];
+                arrOfCards[k] = temp;
+            }
+            foreach (var x in arrOfCards)
+            {
+                deck.Push(x);
+            }
+        }
+
+        //Counts cards (For testing purposes, not illicit purposes)
+        public int CountCards()
+        {
+            return deck.Count;
+        }
+
+        public List<Card> DealPlayerCards()
+        {
+            List<Card> playerCards = new List<Card>();
+            for (int x = 0; x < 2; x++)
+                playerCards.Add(DrawCard());
+
+        }
+
+        //draws card from deck
+        public Card DrawCard()
+        {
+            return deck.Pop();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ LoanShark.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using static PokerGame.HumanPlayer;
+
+namespace PokerGame
+{
+    public class LoanShark
+    {
+        public double loanAmt;
+        const double interestRate = 0.2;
+        public double payment;
+        public int time = 0;
+        public string response;
+
+        public LoanShark()
+        {
+            Console.WriteLine(@"Would you like to take out a loan?" +
+                @"Press ""Y"" for Yes or ""N"" for no.");
+        }
+
+        public void GetLoan(double loanAmt)
+        {
+            this.loanAmt = loanAmt;
+        }
+
+        public bool OfferLoan()
+        {
+            do
+            {
+                GetResponse();
+                VerifyResponse(response);
+
+            } while (!VerifyResponse(response));
+        }
+
+        public void GetResponse()
+        {
+            response = Console.ReadLine();
+            response = response.ToLower();
+        }
+
+        public bool VerifyResponse(string response)
+        {
+            if (response.Length != 1)
+            {
+                Console.WriteLine(@"Please type ""Y"" for yes or ""N"" for no");
+                return false;
+
+            } else if (Convert.ToChar(response) != 'y' || Convert.ToChar(response) != 'n')
+            {
+
+                return false;
+
+            } else
+            {
+                return true;
+            }
+        }
+
+        public void MatureLoan()
+        {
+            loanAmt = interestRate * (1 + interestRate);
+            time++;
+        }
+    }
+}
