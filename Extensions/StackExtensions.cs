90db0c18c48efc6291555da9f66f8dd581243be4
Jake <shanley.jacob@gmail.com>
2019-08-08 19:32:58 -0600

    Check in Chat w/Jake

---
+++ Extensions/StackExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PokerGame.Extensions
+{
+    public static class StackExtensions
+    {
+        public static void ForEach<T>(this Stack<T> myStack, Action<T> action)
+        {
+            foreach(var item in myStack)
+            {
+                action.Invoke(item);
+            }
+        }
+    }
+}
