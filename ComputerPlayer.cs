4c9afe2bd68978f09e1e95c25f2370a95f3405b2
nate-j-nov <51186042+nate-j-nov@users.noreply.github.com>
2019-09-23 22:32:45 -0400

    Added updated PokerGame App
    *GetBestHand is working
    *Can go through an entire hand
    *Created PlayerCommCards which gets rid of the need to have each player read in the cards on the table. It also provides a uniform way in which GetBestHand and PerformTurn are executed. 
    *Needs to determine the winner

--- ComputerPlayer.cs
+++ ComputerPlayer.cs
@@ -6,7 +6,7 @@ using System.Linq;
 namespace PokerGame
 {
     public class ComputerPlayer : Player
-    {
+    { 
         public ComputerPlayer(string playerName) : base(playerName)
         {
 
@@ -21,50 +21,229 @@ namespace PokerGame
 
         private int GetDecisionNumber(out double _raiseAmount)
         {
-            //double raiseAmount; 
-            var myBestHand = GetBestHand(new List<Card>());
+            MyBestHand = GetBestHand(); 
             Random random = new Random();
             var randomDouble = random.NextDouble();
-            switch (myBestHand)
+            var totalHand = new List<Card>();
+            totalHand.AddRange(Hand);
+            totalHand.AddRange(PlayerCommCards); 
+
+            Console.WriteLine(Environment.NewLine + $"{PlayerName} has a {MyBestHand.ToString()}" + Environment.NewLine);
+            
+            switch (MyBestHand)
             {
                 case WinningHands.HighCard:
-                    if (Hand.Max(card => card.Face) <= CardFace.Ten)
+                    if(randomDouble < 0.0005)
+                    {
+                        _raiseAmount = Money;
+                        return 3;
+                    }
+
+                    if (totalHand.Max(card => card.Face) <= CardFace.Ten && OtherPlayersBets > 0.025 * Money)
+                    {
+                        _raiseAmount = 0;
+                        return 1;
+                    }
+                    else
+                    {  
+                            _raiseAmount = 0;
+                            return 2;
+                    }
+                   
+                case WinningHands.Pair:
+                    var pairs = totalHand
+                        .GroupBy(card => card.Face)
+                        .Where(group => group.Count() == 2)
+                        .SelectMany(group => group.Skip(1))
+                        .Max(card => card.Face);
+
+                    if(randomDouble <= 0.001)
+                    {
+                        _raiseAmount = Money;
+                        return 3;
+                    }
+
+                    if (OtherPlayersBets > 0.1 * Money && pairs <= CardFace.Ten)
                     {
                         _raiseAmount = 0;
                         return 1;
                     }
                     else
                     {
+                        if (OtherPlayersBets >= 0.05 * Money && OtherPlayersBets <= 0.1 * Money)
+                        {
+                            _raiseAmount = 0;
+                            return 2;
+                        }
+                        else
+                        {
+                            _raiseAmount = (0.1 * Money) - OtherPlayersBets;
+                            return 3;
+
+                        }
+                    }
+                    
+                case WinningHands.TwoPair:
+                    if(randomDouble < 0.0025)
+                    {
+                        _raiseAmount = Money;
+                        return 3;
+                    }
+                   
+                    if (OtherPlayersBets > 0.2 * Money)
+                    {
+                        _raiseAmount = 0;
+                        return 1;
+                    }
+                    else if(OtherPlayersBets <= 0.2 * Money && OtherPlayersBets > 0.15 * Money)
+                    {
                         _raiseAmount = 0;
                         return 2;
-                    }
-
-                case WinningHands.Pair:
-                    var pairs = Hand
-                        .GroupBy(card => card.Face)
-                        .Where(group => group.Count() == 2)
-                        .SelectMany(group => group.Skip(1))
-                        .Max(card => card.Face);
-                        
-                    if (pairs > CardFace.Ten)
-                    {
-                        _raiseAmount = .1 * Money;
-                        return 3;
                     }
                     else
                     {
+                        _raiseAmount = (0.2 * Money) - OtherPlayersBets; 
+                        return 3;
+                    }
+
+                case WinningHands.ThreeOfAKind:
+                    if(randomDouble < 0.005)
+                    {
+                        _raiseAmount = Money;
+                        return 3;
+                    }
+
+                    if (OtherPlayersBets > 0.3 * Money)
+                    {
+                        _raiseAmount = 0;
+                        return 1;
+                    }
+                    else if (OtherPlayersBets <= 0.3 * Money && OtherPlayersBets >= 0.2 * Money)
+                    {
                         _raiseAmount = 0;
                         return 2;
                     }
+                    else
+                    {
+                        _raiseAmount = (0.25 * Money) - OtherPlayersBets;
+                        return 3;
+                    }
+                    
 
-                case WinningHands.TwoPair:
-                    _raiseAmount = 0.2 * Money;
-                    return 3;
+                case WinningHands.Straight:
+                    if(randomDouble < 0.005)
+                    {
+                        _raiseAmount = Money;
+                        return 3;
+                    }
 
-                case WinningHands.ThreeOfAKind:
-                    _raiseAmount = 0.3 * Money;
+                    if (OtherPlayersBets > 0.32 * Money)
+                    {
+                        _raiseAmount = 0;
+                        return 1;
+                    }
+                    else if(OtherPlayersBets <= 0.32 && OtherPlayersBets > 0.27)
+                    {
+                        _raiseAmount = 0;
+                        return 2;
+                    }
+                    else
+                    {
+                        _raiseAmount = (0.275 * Money) - OtherPlayersBets;
+                        return 3;
+                    }
+
+                case WinningHands.Flush:
+                    if (randomDouble < 0.0075)
+                    {
+                        _raiseAmount = Money;
+                        return 3;
+                    }
+
+                    if(OtherPlayersBets > 0.43 * Money)
+                    {
+                        _raiseAmount = 0;
+                        return 1;
+                    }
+                    else if (OtherPlayersBets <= 0.43 * Money && OtherPlayersBets > 0.38)
+                    {
+                        _raiseAmount = 0;
+                        return 2;
+                    }
+                    else
+                    {
+                        _raiseAmount = (0.38 * Money) - OtherPlayersBets;
+                        return 3;
+                    }
+                    
+
+                case WinningHands.FullHouse:
+                    if(randomDouble < 0.008)
+                    {
+                        _raiseAmount = Money;
+                        return 3;
+                    }
+
+                    if(OtherPlayersBets > 0.5 * Money)
+                    {
+                        _raiseAmount = 0;
+                        return 1;
+                    }
+                    else if (OtherPlayersBets <= 0.5 * Money && OtherPlayersBets > 0.45 * Money)
+                    {
+                        _raiseAmount = 0;
+                        return 2;
+                    }
+                    else
+                    {
+                        _raiseAmount = (0.45 * Money) - OtherPlayersBets;
+                        return 3;
+                    }
+
+                case WinningHands.FourOfAKind:
+                    if(randomDouble < 0.05)
+                    {
+                        _raiseAmount = Money;
+                        return 3;
+                    }
+
+                    if(OtherPlayersBets > 0.60 * Money)
+                    {
+                        _raiseAmount = 0;
+                        return 1;
+                    }
+                    else if(OtherPlayersBets <= 0.60 * Money && OtherPlayersBets > 0.55 * Money)
+                    {
+                        _raiseAmount = 0;
+                        return 2;
+                    }
+                    else
+                    {
+                        _raiseAmount = (0.60 * Money) - OtherPlayersBets;
+                        return 3;
+                    }
+
+                case WinningHands.StraightFlush:
+                    if(randomDouble < 0.5)
+                    {
+                        _raiseAmount = Money;
+                        return 3;
+                    }
+
+                    if (OtherPlayersBets > 0.9 * Money)
+                    {
+                        _raiseAmount = 0;
+                        return 2;
+                    }
+                    else
+                    {
+                        _raiseAmount = (0.9 * Money) - OtherPlayersBets;
+                        return 3;
+                    }
+
+                case WinningHands.RoyalFlush:
+                    _raiseAmount = Money;
                     return 3;
-                
 
                 default:
                     _raiseAmount = 0;
--- HumanPlayer.cs
+++ HumanPlayer.cs
@@ -14,14 +14,17 @@ namespace PokerGame
         public override Decision PerformTurn()
         {
             Console.WriteLine("What would you like to do?\n" +
-               "Press: 1 for Fold \n" +
-               "2 for Call\n" +
+               "Press: " + Environment.NewLine +
+               "1 for Fold" + Environment.NewLine +
+               "2 for Call" + Environment.NewLine +
                "3 for Raise");
 
+            MyBestHand = GetBestHand();
             userChoice = int.Parse(Console.ReadLine()); //Get user input
             DecisionType decision = (DecisionType)userChoice;
             return new Decision(decision);
         }
+
         public override bool VerifyDecision()
         {
             if (userChoice >= 1 && userChoice <= 3)
